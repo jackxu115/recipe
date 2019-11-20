a7690286675f29fe873a5687c1bb5b5abbc29b03
pengxu <pengxu19890115@gmail.com>
2019-11-19 16:58:07 -0800

    delete unused code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 var data = [
   {
@@ -40,24 +38,7 @@ var data = [
 ];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
